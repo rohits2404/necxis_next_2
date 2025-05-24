1dca227a0b8c79109c1cecb1511e5f678f201004
Rohit Sharma <rohits8731@gmail.com>
2025-05-24 12:44:42 +0530

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 "use client"
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
-
-const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
